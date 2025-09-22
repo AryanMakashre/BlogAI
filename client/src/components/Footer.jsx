5d790a3c8b96822e5ac4aa5a27c3821b1a25b4da
Aryan Makashre <aryanmakashre.777@gmail.com>
2025-09-22 13:05:29 +0530

    Initial commit – BlogAI project

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { assets, footer_data } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+        <div>
+          
+          <p className="max-w-[410px] mt-6">
+            BlogAI – AI-powered Blogging Platform
+Build, manage, and publish blogs easily.
+Admin dashboard and AI content generation included
+          </p>
+        </div>
+
+        <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+          {footer_data.map((section, index) => (
+            <div key={index}>
+              <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">
+                {section.title}
+              </h3>
+              <ul className="text-sm space-y-1">
+                {section.links.map((link, i) => (
+                  <li key={i}>
+                    <a href="#" className="hover:underline transition">
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
+        BlogAI 
+         AI-Powered Blogging
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { assets } from "../assets/assets.js";
+
+import { useAppContext } from "../context/AppContext.jsx";
+
+const Navbar = () => {
+  const { navigate, token } = useAppContext();
+
+  return (
+    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 ">
+      <div
+        className="flex items-center gap-2 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        <img
+          src={"/blogbytes_logo.svg"}
+          alt="BlogAI Logo"
+          className="w-10 h-10 sm:w-12 sm:h-12"
+        />
+        <span className="text-2xl sm:text-3xl font-bold text-primary tracking-tight">
+          BlogAI
+        </span>
+      </div>
+
+      <button
+        onClick={() => navigate("/admin")}
+        className="flex items-center gap-2 rounded-full text-sm cursor-pointer  bg-primary text-white px-10 py-2.5 "
+      >
+        {token ? "Dashboard" : "Login"}
+        <img src={assets.arrow} className="w-3" alt="arrow" />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
